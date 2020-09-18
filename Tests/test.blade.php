693b886815ba81e8874791d412b805c544a9befa
Melvin Beemer <melvin@hey.com>
2020-09-18 13:06:27 +0000

    Add support for components

--- Tests/test.blade.php
+++ Tests/test.blade.php
@@ -6,7 +6,7 @@
 	
 	@auth
 		<nav>
-			<a href="{{ route('account') }}">Account</a>
+			<a href="{{ route('account') }}" @if($route->selected)checked="asdf"@endif>Account</a>
 		</nav>
 	@endauth
 	
@@ -15,4 +15,11 @@
 			<h1><a href="{{ route('articles.show', ['id' => $article->id]) }}">{{ $article->title }}</a></h1>
 		</article>
 	@endforeach
+	
+	<x-test-test.test test="test">
+		@if($test)
+			<div @if($test)checked="checked"@endif></div>
+			<div @if($test->property)checked="checked"@endif></div>
+		@endif
+	</x-test-test.test>
 @endsection
