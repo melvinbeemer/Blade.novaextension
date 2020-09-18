80005109517d781e162c97cbd8c3edafc4d9ff50
Melvin Beemer <melvin@hey.com>
2020-09-18 05:53:48 +0000

    Much improved syntax highlighting

--- Tests/test.blade.php
+++ Tests/test.blade.php
@@ -1,22 +1,18 @@
-<h1>This is a title</h1>
-<p>This is a text supporting the title.</p>
+@extends('app')
 
-{{ $test }}
-
-{!! if($name == "test") !!}
-
-@guest
-	<div></div>
-	{{ $test }}
-@endguest
-
-@foreach
-	@if
-		<div>test</div>
-		
-		@yield('')
-		<div>
-			{{ $name }}
-		</div>
-	@endif
-@endforeach
+@section('content')
+	<h1>Page title: {{ $title }}</h1>
+	<p>This is a text supporting the title.</p>
+	
+	@auth
+		<nav>
+			<a href="{{ route('account') }}">Account</a>
+		</nav>
+	@endauth
+	
+	@foreach($articles as $article)
+		<article>
+			<h1><a href="{{ route('articles.show', ['id' => $article->id]) }}">{{ $article->title }}</a></h1>
+		</article>
+	@endforeach
+@endsection
